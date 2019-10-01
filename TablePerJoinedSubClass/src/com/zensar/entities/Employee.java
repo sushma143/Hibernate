36150551fcac011ca570db4c41f3db3c515825a4
sushma143 <sushmasinghjk@gmail.com>
2019-10-01 12:31:45 +0530

    Added TablePerJoinedClass

---
+++ TablePerJoinedSubClass/src/com/zensar/entities/Employee.java
@@ -0,0 +1,89 @@
+package com.zensar.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+/**
+ * @author Sushma Kumari
+ * @Creation_date 27th Sept 2019 11:26
+ * @version 3 .0
+ * @modification_date 27th Sept 2019 2:42 PM
+ * @Description It is a Persistent Class.
+ * 				It is a POJO class. 
+ *
+ */
+@Entity
+@Table(name="Emp_Master2")
+@Inheritance(strategy = InheritanceType.JOINED)
+
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+	private LocalDate joinDate;
+	private double salary;
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(int id, String name, LocalDate joinDate, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.joinDate = joinDate;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(LocalDate joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", joinDate=" + joinDate + ", salary=" + salary + "]";
+	}
+	
+	
+
+}
